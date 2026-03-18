67693fc1c362b38b2960b03882298fcad8d4f17d
Shreya19-777 <shreyaprasad777@gmail.com>
2026-03-18 12:44:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,56 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import tkinter as tk
-from tkinter import ttk
+
+#Using customtkinter instead for easier design
+import customtkinter as ctk
+
 from scipy import signal
 
 #GUI for the user to input csv filename, columns to plot and the data type
-class GUI:
-    def __init__(self):
-        self.window = tk.Tk()
-        self.window.title("SYSC 2010 Final Project")
-        self.window.geometry("520x380")
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")
 
+class GUI(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("SYSC 2010 Final Project")
+        self.geometry("500x550")
+        
         #Getting CSV file name
-        tk.Label(self.window, text="CSV File Name", font=('Arial', 11)).pack()
-        self.entry_file = tk.Entry(self.window, width=40, font=('Arial', 11))
-        self.entry_file.pack(pady=4)
+        self.label_title = ctk.CTkLabel(self, text="CSV File Name", font=ctk.CTkFont(size=20, weight="bold")).pack()
+        self.entry_file = ctk.CTkEntry(self, width=300, font=('Arial', 11))
+        self.entry_file.pack(pady=5)
 
         #Getting the x axis column name
-        tk.Label(self.window, text="X-axis Column Name", font=('Arial', 11)).pack()
-        self.entry_x = tk.Entry(self.window, width=40, font=('Arial', 11))
-        self.entry_x.pack(pady=4)
+        ctk.CTkLabel(self, text="X-axis Column (Time): ", font=('Arial', 11)).pack()
+        self.entry_x = ctk.CTkEntry(self, width=300)
+        self.entry_x.pack(pady=5)
 
         #Getting y axis column name
-        tk.Label(self.window, text="Y-axis Column Name", font=('Arial', 11)).pack()
-        self.entry_y = tk.Entry(self.window, width=40, font=('Arial', 11))
-        self.entry_y.pack(pady=4)
-
-        #Data type options
-        data_options = ["ECG", "Temperature", "Respiration", "Motion"]
+        ctk.CTkLabel(self, text="Y-axis Column (Signal): ").pack()
+        self.entry_y = ctk.CTkEntry(self, width=300)
+        self.entry_y.pack(pady=5)
 
         #Dropdown list
-        tk.Label(self.window, text="Select Data Type:").pack(pady=(10, 0))
-        self.selected_type = tk.StringVar()
-        self.dropdown = ttk.Combobox(self.window, textvariable=self.selected_type, values=data_options,state="readonly")
-        self.dropdown.pack(pady=5)
-        
-        # Set a default value so it isn't initially empty
-        self.dropdown.current(0) 
+        self.label_type = ctk.CTkLabel(self, text="Select Data Type:")
+        self.label_type.pack(pady=(15, 0))
+        self.dropdown = ctk.CTkComboBox(self, values=["ECG", "Temperature", "Respiration", "Motion"], width=200)
+        self.dropdown.set("ECG") # Default value
+        self.dropdown.pack(pady=10)
 
         #Button to plot the initial data
-        tk.Button(self.window, text="Load & Filter", font=('Arial', 11),command=self.handle_selection,width=15).pack()
-        self.window.mainloop()
+        self.btn_load = ctk.CTkButton(self, text="Load & Filter Data", 
+            command=self.handle_selection,
+            fg_color="transparent", 
+            border_width=2,
+            text_color=("gray10", "#DCE4EE"))
+        self.btn_load.pack(pady=30)
     
     def handle_selection (self) :
         #Saving the user inputted data type
-        choice = self.selected_type.get()
+        choice = self.dropdown.get()
         print(f"User chose {choice}")
 
         #Data used to plot unfiltered data
@@ -53,20 +58,29 @@ class GUI:
         x = self.entry_x.get().strip()
         y = self.entry_y.get().strip()
 
+        #Reading csv and plotting the data
         try:
             df = pd.read_csv(filename)
             df = df.interpolate(method='linear')
             
             time = np.array(df[x])
             signal_data = np.array(df[y])
+            
+            #Getting the sampling frequency for the data
+            # Ensure the time column is in datetime format
+            df[x] = pd.to_datetime(df[x])
 
-            plt.figure(figsize=(14,6))
+            #Time difference between consecutive rows
+            time_diff = df[x].diff().mean()
+            
+            fs = 1 / time_diff.total_seconds()
+            
+            plt.figure(figsize=(10,8))
             plt.plot(df[x], df[y], color='pink')
             plt.title(f"Unfiltered plot of {y} vs {x}")
             plt.xlabel(x)
             plt.ylabel(y)
             plt.grid(True)
-            plt.tight_layout()
             plt.show()    
             
             #Calling function for displaying the key features of the unfiltered data
@@ -74,41 +88,35 @@ class GUI:
             print(features)
             
             #Applying correct filter for the given data type
-            apply_filter(choice, signal_data, time) 
+            apply_filter(choice, signal_data, time, fs) 
                 
         #Error message for incorrect inputted values from user
         except Exception as e:
             print("Error", str(e))
         
 #**************************************CHOOSING CORRECT FILTER FOR SPECIFIED DATA TYPE****************************************
-def apply_filter(choice, signal_data, t) :
+def apply_filter(choice, signal_data, t, fs) :
     #ECG Signal:
     if choice == "ECG":
         print("Applying ECG Bandpass Filter")
-        ecg_bandpass_filter(500, signal_data, t) 
+        ecg_bandpass_filter(fs, signal_data, t) 
     #Temperature:
     elif choice == "Temperature" :
         print("Applying Temperature Filter")
-        filtered_temp = temp_lowpass_filter(0.1, signal_data, order=5)
+        filtered_temp = temp_lowpass_filter(fs, signal_data, order=5)
     #Motion
     elif choice == "Motion":
         print("Applying Motion Median Filter")
-        motion_comp_filter(100, signal_data)
+        motion_comp_filter(fs, signal_data)
     #Respiration
     elif choice == "Respiration" :
         print("Applying Respiration Filter")
-        resp_bandpass_filter(25, signal_data, t)
+        resp_bandpass_filter(fs, signal_data, t)
     #No choice
     else :
         print("ERROR, no choice")
         
 #Applying the different filters for each type of signal
-def temp_lowpass_filter(data, cutoff, fs, order):
-    nyquist = 0.5 * fs
-    normal_cutoff = cutoff / nyquist
-    np.convolve()
-    b, a = signal.butter(order, normal_cutoff, btype='low', analog=False)
-    return signal.filtfilt(b, a, data)
 
 def ecg_bandpass_filter(fs, signal_data, t) :
     minimum = 0.5
@@ -121,6 +129,14 @@ def ecg_bandpass_filter(fs, signal_data, t) :
     plt.ylabel("Amplitude")
     plt.plot(t, bp_filtered_ecg, color='purple')
     plt.show()
+    
+    return bp_filtered_ecg
+    
+def temp_lowpass_filter(data, cutoff, fs, order):
+    nyquist = 0.5 * fs
+    normal_cutoff = cutoff / nyquist
+    b, a = signal.butter(order, normal_cutoff, btype='low', analog=False)
+    return signal.filtfilt(b, a, data)
     
 def resp_bandpass_filter(fs, signal_data, t) :
     minimum = 0.1
@@ -137,7 +153,7 @@ def resp_bandpass_filter(fs, signal_data, t) :
     
     return bp_filtered_resp
     
-def motion_comp_filter(fs, signal_data) :
+def motion_comp_filter(fs, signal_data, t) :
     median_filtered = signal.medfilt(signal_data, kernel_size=5)
     
     cutoff = 5.0 
@@ -151,7 +167,7 @@ def motion_comp_filter(fs, signal_data) :
     plt.title("Respirarion Filtered Signal")
     plt.xlabel("Time (s)")
     plt.ylabel("Amplitude")
-    plt.plot(t, bp_filtered_resp, color='orange')
+    plt.plot(t, filtered_motion, color='orange')
     plt.show()
     
     return filtered_motion
@@ -174,3 +190,4 @@ def key_features(signal_data):
 
 if __name__ == "__main__":
     app = GUI()
+    app.mainloop()
