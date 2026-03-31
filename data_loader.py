35867721559a210113e9b4b949cfd61064e779ce
Shreya19-777 <shreyaprasad777@gmail.com>
2026-03-31 15:12:57 -0400

    did nothing

--- data_loader.py
+++ data_loader.py
@@ -30,6 +30,7 @@ def data_load(filename, choice, x, y):
             return None, None
         
         time = np.array(df[x])
+            
         signal_data = np.array(df[y])
         
         #Only call preprocess if there are no errors with the csv
